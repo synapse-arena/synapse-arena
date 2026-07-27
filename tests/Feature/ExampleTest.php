d1887fb1088ce98fb38c9ed7d3bc0d644216f8f3
nathAlv08 <150881129+nathAlv08@users.noreply.github.com>
2026-07-27 21:23:27 +0700

    Refactor ExampleTest to use it() syntax

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,27 +1,8 @@
 <?php
 
-<<<<<<< HEAD
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
-=======
 it('returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
 });
->>>>>>> 42e2ef447ae4d631f693b47ae4dc0d0b538ab45b
+
