f192c05a48463a16c57cbc5929786966a48785c9
nathAlv08 <150881129+nathAlv08@users.noreply.github.com>
2026-07-27 21:22:46 +0700

    Replace old test with new true assertion test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,7 +1,6 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
+test('that true is true', function () {
+    expect(true)->toBeTrue();
 });
+
