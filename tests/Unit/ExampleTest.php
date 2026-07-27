33c4d94a55bd4a52f38ef0e61c910488b269d340
nathAlv08 <150881129+nathAlv08@users.noreply.github.com>
2026-07-27 21:21:30 +0700

    Refactor ExampleTest to use new syntax
    Refactor ExampleTest to use the new testing syntax and check for a successful response.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,22 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-namespace Tests\Unit;
+it('returns a successful response', function () {
+    $response = $this->get('/');
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
-=======
-test('that true is true', function () {
-    expect(true)->toBeTrue();
+    $response->assertStatus(200);
 });
->>>>>>> 42e2ef447ae4d631f693b47ae4dc0d0b538ab45b
